2dce99a3ffb798515f31266e8a9176c6f3e3d383
Milly <35267727+mimilmilly@users.noreply.github.com>
2025-06-12 20:04:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const email = e.target[0].value;
-    const password = e.target[1].value;
-  
+  e.preventDefault();
+  const email = e.target[0].value.trim();
+  const password = e.target[1].value.trim();
+
   if (email && password) {
     alert("Login Successful");
   } else {
     alert("Please fill in all fields.");
   }
-  });
+});
   
   // Modal functionality
   const modal = document.getElementById("modal");
